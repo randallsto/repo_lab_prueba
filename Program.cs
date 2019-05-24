55c668c798c13446c01aae2b6e86f175aaf53780
Randall <randall2587@gmail.com>
2019-05-23 18:28:01 -0600

    creacion de nueva app de consola

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace repo_lab_prueba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
